09bf1367a11aadc9628a4b122a34d4a2567d55ab
astulgate <astulgate@gmail.com>
2023-02-17 09:13:48 -0600

    adding data persistence
    adding data persistence between scenes

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+
+    public static DataManager Instance;
+
+    public string playerName;
+    public int highScore;
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public string playerName;
+        public int highScore;
+    }
+}
